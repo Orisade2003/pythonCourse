d68ff48d42860d3b4e7d8e5b614d8a8ef0c97ffe
Orisade <orisade2003@gmail.com>
2022-02-20 18:16:22 +0200

    finished

--- Exercises/ex6.2.4.py
+++ Exercises/ex6.2.4.py
@@ -4,10 +4,12 @@ def extend_list_x(list_x, list_y):
     :param list_y: the list you want to add to list_x
     :return: a new list which adds list_y at the start of list_X
     """
-    list_x = [*list_y, *list_x]
+    list_x[:0] = list_y
     return list_x
+
+
 if __name__ == '__main__':
-    l = [5,6,7,8]
-    l2 = [1,2,3,4]
-    l =extend_list_x(l,l2)
+    l = [5, 6, 7, 8]
+    l2 = [1, 2, 3, 4]
+    extend_list_x(l, l2)
     print(l)
---
+++ Exercises/ex7.2.1.py
@@ -0,0 +1,24 @@
+def is_greater(my_list, n):
+    """
+
+    :param my_list: a list given by the user
+    :type my_list: list
+    :param n: a number given by the user
+    :type n: int
+    :return: the function return a new list which contains all the numbers which are in the original list
+            and are larger than n
+    """
+    new_list = []
+    for i in my_list:
+        if i > n:
+            new_list.append(i)
+    return new_list
+
+
+def main():
+    my_list = [42,13,43,54,12,4]
+    new_l = is_greater(my_list,10)
+    print(new_l)
+
+if __name__ == '__main__':
+    main()
